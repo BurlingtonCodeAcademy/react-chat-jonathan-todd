3d5ff8adc27ffcd1c08e27e849c6ca93bbc6eea0
JT802 <jonathan.tresch@gmail.com>
2020-10-28 17:01:38 -0400

    Wednesday 10/28 basic push

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
+//immport the required things
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
-
 const port = process.env.PORT || 8000;
 const app = express();
 const staticDir = process.env.DEV ? "./client/public" : "./client/build";
 
+//middleware
 app.use(express.static(staticDir));
+app.use(express.urlencoded({extended: true}))
 
+//Database integration (Jonathan needs his data-store.js file)
+const DataStore = require("./data-store.js");
+let myDb = new DataStore (`mongodb+srv://Jonathan:${process.env.DBPASS}@cluster0.fmoix.mongodb.net/<dbname>?retryWrites=true&w=majority`)
 
+//API endpoint goes here ...
 
+//creating a catch-all route
+app.get('*', (request, response) => {
+  response.sendFile(path.resolve(staticDir + '/index.html'))
+})
+//what port we're listening on
 app.listen(port, () => {
   console.log('listening on port: ' + port) 
 })
