3aff21dda5599d61ea61bce1d85f1bd84dc0ead4
toddkowalczyk <toddmkowalczyk@gmail.com>
2020-10-29 08:39:38 -0400

    established connection to Atlas DB

---
+++ data.js
@@ -0,0 +1,52 @@
+const { ObjectId, MongoClient } = require('mongodb')
+
+class DataStore {
+    constructor(url, dbName, collectionName) {
+        this.url = url
+        this.dbName = dbName
+        this.collName = collectionName
+        this.connection = null
+
+        console.log('url: ', url)
+        console.log('dbName: ', dbName)
+        console.log('collection: ', collectionName)
+    }
+
+    async connect() {
+
+        // check if connection already exists
+        if (this.connection && this.connection.isConnected()) {
+            console.log('already exists')
+            return this.connection
+
+            // if not, create connection and return it
+        } else {
+            console.log('creating a new connection')
+            const client = await MongoClient.connect(this.url, { useUnifiedTopology: true })
+            this.connection = client
+            return this.connection
+        } 
+    }
+
+    async readData() {
+        let client = await this.connect()
+        console.log('client: ', client)
+        let db = await client.db(this.dbName)
+        console.log('db: ', db)
+        const collection = db.collection(this.collName)
+        console.log('collection: ', collection)
+        let dataArr = await collection.find({}).toArray()
+        console.log('dataArr: ', dataArr)
+        return dataArr
+    }
+
+    async addOne(entryObj) {
+        let client = await this.connect()
+        let db = await client.db(this.dbName)
+        let collection = await db.collection(this.collName)
+
+        collection.insertOne(entryObj)
+    }
+}
+
+module.exports = DataStore
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-//immport the required things
+// Import Required Files
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
@@ -6,21 +6,40 @@ const port = process.env.PORT || 8000;
 const app = express();
 const staticDir = process.env.DEV ? "./client/public" : "./client/build";
 
-//middleware
+// Middleware
 app.use(express.static(staticDir));
 app.use(express.urlencoded({extended: true}))
 
-//Database integration (Jonathan needs his data-store.js file)
-const DataStore = require("./data-store.js");
+// Database integration (Jonathan needs his data-store.js file)
+const DataStore = require("./data.js");
 let myDb = new DataStore (`mongodb+srv://Jonathan:${process.env.DBPASS}@cluster0.fmoix.mongodb.net/<dbname>?retryWrites=true&w=majority`)
 
-//API endpoint goes here ...
+let chatDataStore = new DataStore("mongodb+srv://Jonathan:"+ process.env.DBPASS+"@cluster0.fmoix.mongodb.net/jonathan-todd-chat?retryWrites=true&w=majority", "jonathan-todd-chat", "chat-entries")
+console.log(chatDataStore.collection)
 
-//creating a catch-all route
-app.get('*', (request, response) => {
-  response.sendFile(path.resolve(staticDir + '/index.html'))
-})
-//what port we're listening on
+// Listen for incoming messages
 app.listen(port, () => {
   console.log('listening on port: ' + port) 
+})
+
+// --------------------- ROUTES -------------------------------
+
+// Route to allow chat messages to be saved to DB
+app.post('/create', (req, res) => {
+  let submission = req.body
+  chatDataStore.addOne(submission)
+  res.send('Form submitted successfully')
+})
+
+// Route to read chat messages in DB
+app.get('/get', async (req, res) => {
+  let data =  await chatDataStore.readData()
+  console.log('Data is: ', data)
+  res.send(data)
+}) 
+
+// Creating a catch-all route
+// This route needs to ber last
+app.get('*', (request, response) => {
+  response.sendFile(path.resolve(staticDir + '/index.html'))
 })
