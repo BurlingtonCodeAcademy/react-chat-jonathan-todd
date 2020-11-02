816dbcd6284891153ab15350b754d16a26abcf92
JT802 <jonathan.tresch@gmail.com>
2020-11-02 06:32:19 -0500

    reorganization

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
-import Main from "./MainRoom";
-import PostForm from "./PostForm";
-import Channel from "./Channel";
+import Main from "./components/MainRoom";
+import PostForm from "./components/PostForm";
+import Channel from "./components/Channel";
 import "./App.css";
-//import more room components like line 2 (perhaps just two more)
-//install & import react-router-dom ???
+
 
 function App() {
 	return (
