6c972bf19ebd37635f4ff73f181f229cce2d9063
toddkowalczyk <toddmkowalczyk@gmail.com>
2020-11-03 14:21:22 -0500

    fixed merge conflict

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,11 @@
-<<<<<<< HEAD
-import React from "react";
+import React, {useState } from "react";
 import Main from "./components/MainRoom";
-import PostForm from "./components/PostForm";
-import Channel from "./components/Channel";
 import "./App.css";
-
-=======
-import React, {useState} from "react";
-import Main from "./MainRoom";
-import "./App.css";
->>>>>>> 6e48fc66e9dddfbbc3c0c26cf2579aea4ffaa0ba
 
 function App() {
+
+	// A cookie is being used to preserve the channel a user is in 
+//	let channel = document.cookie.split('; ').find(row => row.startsWith('channel')).split('=')[1];
 
 	// Setup state variables to track channel, default user to general channel when the app. starts
 	const [channelSelected, setChannelSelected] = useState('general')
